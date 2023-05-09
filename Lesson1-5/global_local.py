f4ad6790166ccf336662e8ce885af32d8a280d8a
s21015 <s21015@std.it-college.ac.jp>
2023-05-09 03:43:34 +0000

    P36~P42までの内容

---
+++ Lesson1-5/global_local.py
@@ -0,0 +1,12 @@
+total = 0
+def test_func():
+    global total
+    loops = 20
+    for i in range(loops):
+        total = 20
+        for i in range(loops):
+            total += 10
+
+print("totalの初期値", total)
+test_func()
+print("関数実行後のtotalの値", total)

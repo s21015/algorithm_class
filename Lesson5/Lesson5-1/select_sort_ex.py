7a869636098e452d186160e376101bd2075de5f8
s21015 <s21015@std.it-college.ac.jp>
2023-06-13 09:59:58 +0900

    P169までの内容

--- Lesson5/Lesson5-1/select_sort_ex.py
+++ Lesson5/Lesson5-1/select_sort_ex.py
@@ -1,9 +1,16 @@
+# data = [
+#     "miku","nganomei","asidamana","sudamasaki","nagasawamasami(担任)",
+#     "tanakakei","hosinogen","gakkii","satouken","abehirosi",
+#     "murotuyosi","ikura","tyanmina","aiko","matumotohitoshi(校長)",
+#     "devi(教頭)","ikutaerika","hiroyuki","yamadatayuki","imadamio",
+#     "yamauti","kitamuratakumi","hikorohi-"
+# ]
 data = [
-    "miku","nganomei","asidamana","sudamasaki","nagasawamasami(担任)",
-    "tanakakei","hosinogen","gakkii","satouken","abehirosi",
-    "murotuyosi","ikura","tyanmina","aiko","matumotohitoshi(校長)",
-    "devi(教頭)","ikutaerika","hiroyuki","yamadatayuki","imadamio",
-    "yamauti","kitamuratakumi","hikorohi-"
+    "みく","ながのめい","あしだまな","すだまさき",
+    "たなかけい","ほしのげん","がっきー","さとうけん","あべひろし",
+    "むろつよし","いくら","ちゃんみな","あいこ",
+    "いくたえりか","ひろゆき","やまだたかゆき","いまだみお",
+    "やまうち","きたむらたくみ","ひころひー"
 ]
 print(data, "元のデータ")
 
@@ -14,4 +21,7 @@ for i in range(len(data)):
             m = j
     data[i], data[m] = data[m], data[i]
 
-print(data, "ソート後のデータ")
+print(data[0],data[1],data[2],data[3],data[4])
+print(data[5],data[6],data[7],data[8],data[9])
+print(data[10],data[11],data[12],data[13],data[14])
+print(data[15],data[16],data[17],data[18],data[19])

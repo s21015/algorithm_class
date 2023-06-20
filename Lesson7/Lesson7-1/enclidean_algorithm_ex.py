efc37eb8786f53a3ae872d3fbe392a07e4ec1a65
s21015 <s21015@std.it-college.ac.jp>
2023-06-20 11:52:57 +0900

    オリジナル問題を追加

---
+++ Lesson7/Lesson7-1/enclidean_algorithm_ex.py
@@ -0,0 +1,12 @@
+def enclid(a, b):
+    if b == 0:
+        return a
+    else:
+        return enclid(b, a%b)
+
+print("a≧bとなる自然数を入力してください")
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+n = enclid(a, b)
+print("それらの数の最大公約数は", enclid(n, c))
